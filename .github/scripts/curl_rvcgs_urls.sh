54ae46e8909d72abc740bcfa2666f38e60060fa8
marq4 <elapamarquina@hotmail.com>
2025-12-10 20:15:58 -0600

    SHOULD SUC (no www checks).

--- .github/scripts/curl_rvcgs_urls.sh
+++ .github/scripts/curl_rvcgs_urls.sh
@@ -1,7 +1,33 @@
 #! /bin/bash
 
-#echo 'failed=true' >> $GITHUB_OUTPUT
+set -euo pipefail
 
-echo 'failed_urls=www.example.com/fails' >> $GITHUB_OUTPUT
+URLS=(
+    
+    'https://randomvideoclipgenerator.com'
+    
+    'http://randomvideoclipgenerator.com'
+)
+Failed_urls=()
 
-exit 1
+for url in "${URLS[@]}"
+do
+    echo "Checking ${url}... "
+    return_status=$(curl --fail --silent --output /dev/null \
+        --write-out "%{http_code}" --max-time 10 "${url}")
+    if [[ "${return_status}" -eq 200 || "${return_status}" -eq 301 ]]
+    then
+        echo "URL ${url}: OK (${return_status}). "
+    else
+        Failed_urls+=("${url}")
+        echo "URL ${url} not 200: ${return_status}. "
+    fi
+done
+
+if [[ ${#Failed_urls[@]} -gt 0 ]]
+then
+    echo "failed_urls=${Failed_urls[*]}" >> $GITHUB_OUTPUT
+    exit 1
+fi
+
+exit 0
