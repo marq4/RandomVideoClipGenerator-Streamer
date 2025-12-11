fad1bfcf999f3af0fa7b02c437239668c5f54f40
marq4 <elapamarquina@hotmail.com>
2025-12-11 11:06:41 -0600

    Make sure workflow/job/step fails with malformed JSON.

--- .github/scripts/send_notification.sh
+++ .github/scripts/send_notification.sh
@@ -2,27 +2,20 @@
 
 set -euo pipefail
 
-echo "version======>>>>>> 10:39:30" #TMP
-
 failed_urls_string=$1
-echo -e "\n\n\nfailed_urls_string=>${failed_urls_string}<=\n\n" #TMP
 discord_webhook_url=$2
-#$(echo "${Failed_urls}" | tr ' ' '\n' | sed 's/^/• /')
 
 # Convert space-separated string to array:
 IFS=' ' read -ra URL_Array <<< "${failed_urls_string}"
-echo -e "\n\nURL_Array=>${URL_Array[@]}<=\n\n" #TMP
 
 unreachable_urls_string_with_newlines=""
 for url in "${URL_Array[@]}"
 do
-    echo -e "\n    >>>>>FOR->${url}<\n" #TMP
     unreachable_urls_string_with_newlines="${unreachable_urls_string_with_newlines}• ${url}\n"
 done
-echo  -e "\n\n\n_______ unreachable_urls_string_with_newlines=>${unreachable_urls_string_with_newlines}<=" #TMP
 
 
-curl --silent --show-error --location --write-out "\n%{http_code}" \
+response=$(curl --silent --show-error --location --write-out "\n%{http_code}" \
     -H 'Content-Type: application/json' \
     -d @- "${discord_webhook_url}" <<EOM
 {
@@ -36,24 +29,23 @@ curl --silent --show-error --location --write-out "\n%{http_code}" \
             "value": "${unreachable_urls_string_with_newlines}",
             "inline": false
         }]
-    }]
+    }];;;
 }
 EOM
-#)
+)
 
+http_code=$(echo "$response" | tail -n1)
+response_body=$(echo "$response" | head -n-1)
 
-#http_code=$(echo "$response" | tail -n1)
-#response_body=$(echo "$response" | head -n-1)
+echo "Discord API response: $http_code:"
+echo "$response_body"
 
-#echo "Discord API response: $http_code:"
-#echo "$response_body"
-
-#if [[ "${http_code}" -ge 200 && "${http_code}" -lt 300 ]]
-#then
-#    echo "Discord notification sent successfully. "
-#else
-#    echo "Failed to send Discord notification. "
-#    exit 1
-#fi
+if [[ "${http_code}" -ge 200 && "${http_code}" -lt 300 ]]
+then
+    echo "Discord notification sent successfully. "
+else
+    echo "Failed to send Discord notification. "
+    exit 1
+fi
 
 exit 0
