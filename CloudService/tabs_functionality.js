ee97ccfb55cfa77ed9e690daf48347a8da93f633
marq4 <elapamarquina@hotmail.com>
2025-12-12 12:51:31 -0600

    JS lint fixes...

--- CloudService/tabs_functionality.js
+++ CloudService/tabs_functionality.js
@@ -3,9 +3,9 @@ const contents = document.querySelectorAll('.tab-content');
 
 buttons.forEach(btn => {
   btn.addEventListener('click', () => {
-      buttons.forEach(b => b.classList.remove('active'));
-      btn.classList.add('active');
-      contents.forEach(c => c.classList.add('hidden'));
-      document.getElementById(btn.dataset.tab).classList.remove('hidden');
+    buttons.forEach(b => b.classList.remove('active'));
+    btn.classList.add('active');
+    contents.forEach(c => c.classList.add('hidden'));
+    document.getElementById(btn.dataset.tab).classList.remove('hidden');
   });
 });
