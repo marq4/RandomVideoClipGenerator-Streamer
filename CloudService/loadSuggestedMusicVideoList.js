ae03e3f694a944947861b91b43ce1fa4194a21b9
marq4 <elapamarquina@hotmail.com>
2025-12-26 10:14:58 -0600

    JS lint.

--- CloudService/loadSuggestedMusicVideoList.js
+++ CloudService/loadSuggestedMusicVideoList.js
@@ -1,4 +1,6 @@
-async function getSuggestedMusicVideoListJSON() {
+/* eslint-env browser */
+
+async function getSuggestedMusicVideoListJSON () {
   try {
     const response = await fetch('https://swo0pk82b9.execute-api.us-east-2.amazonaws.com/prod/list')
     if (!response.ok) {
@@ -15,13 +17,13 @@ async function getSuggestedMusicVideoListJSON() {
 document.getElementById('loadSuggestedMusicVideoList').addEventListener('click', async () => {
   try {
     const musicVideoListJSON = await getSuggestedMusicVideoListJSON()
-    console.log(musicVideoListJSON) //TMP
+    console.log(musicVideoListJSON) // TMP
     const container = document.getElementById('musicVideoListContainer')
     container.innerHTML = '<p>Music Video List will be rendered here!</p>'
   } catch (error) {
     // Handle error in UI:
-    const msg = `Failed to load videos. ` +
-                `I will greatly appreciate it if you contact me and let me know.`
-    alert(msg);
+    const msg = 'Failed to load videos. ' +
+                'I will greatly appreciate it if you contact me and let me know.'
+    alert(msg)
   }
 })
